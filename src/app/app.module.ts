5655b6f17ae09eac6d95c2916019be8639638aab
Saravanan <hsaravanan29@gmail.com>
2020-07-08 13:53:19 +0530

    app routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import 'hammerjs';
+
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishService } from './services/dish.service';
